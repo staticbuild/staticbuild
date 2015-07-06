caf809ae4ad8bcc57d3de5957f76c80d621432fd
Wayne Bloss <waynebloss@gmail.com>
2015-07-05 23:08:55 -0400

    Setting up yargs.

---
+++ test/try-yargs.js
@@ -0,0 +1,58 @@
+﻿'use strict';
+
+var pkg = require('../package.json');
+
+var argv = require('yargs')
+.wrap(null)
+
+.usage(
+  pkg.name + ' v' + pkg.version + ' - ' + pkg.description + 
+  '\nUsage:\n  ' + pkg.name + ' <command> [options] [path]'
+)
+
+.version(
+  pkg.name + ' v' + pkg.version, 'v', 
+  'Show version number.')
+.alias('v', 'version')
+
+.command('dev', 'Run the development web server.', 
+function (yargs) {
+  
+  argv = yargs
+  .wrap(null)
+  .usage(
+    pkg.name + ' v' + pkg.version + ' - Development server.\n' + 
+  '\nUsage:\n  ' + pkg.name + ' dev [options] [path]'
+  )
+  .option('n', {
+    alias: 'norestart',
+    description: 'Disables the built-in nodemon server restart.',
+    type: 'boolean'
+  })
+  .option('r', {
+    alias: 'restart-delay',
+    description: 'Number of seconds to delay nodemon restarts.',
+    type: 'number'
+  })
+  .help('h', 'Show help.').alias('h', 'help')
+  .epilogue('  path           Path to a staticbuild.json file or directory to find one.\n' +
+'                 If no path is supplied, the current directory is used.')
+  .argv;
+
+  console.log('running dev server...');
+
+})
+
+.command('setup', 'Setup a new project.')
+
+.option('V', {
+  alias: 'verbose',
+  description: 'Enables verbose output.'
+})
+
+.help('h', 'Show help.').alias('h', 'help')
+.epilogue('  path           Path to a staticbuild.json file or directory to find one.\n' +
+'                 If no path is supplied, the current directory is used.')
+.argv;
+
+console.dir(argv);
