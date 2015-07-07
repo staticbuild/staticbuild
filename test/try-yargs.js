4ff90eb9a9ec4a8239c054db2efbd94ec4da919a
Wayne Bloss <waynebloss@gmail.com>
2015-07-07 18:40:11 -0400

    Organized yargs code.

--- test/try-yargs.js
+++ test/try-yargs.js
@@ -1,29 +1,38 @@
 ﻿'use strict';
 
+// #region Imports
+var Yargs = require('yargs');
+// #endregion
+
 var pkg = require('../package.json');
+var argv;
 
-var argv = require('yargs')
-.wrap(null)
+function run() {
+  argv = configureYargs()
+  .usage(mainUsage())
+  .version(titleVersion(), 'v', 'Show version number.')
+  .alias('v', 'version')
 
-.usage(
-  pkg.name + ' v' + pkg.version + ' - ' + pkg.description + 
-  '\nUsage:\n  ' + pkg.name + ' <command> [options] [path]'
-)
+  .command('dev', 'Run the development web server.', runDevServer)
 
-.version(
-  pkg.name + ' v' + pkg.version, 'v', 
-  'Show version number.')
-.alias('v', 'version')
+  .command('setup', 'Setup a new project.', runSetup)
 
-.command('dev', 'Run the development web server.', 
-function (yargs) {
-  
-  argv = yargs
-  .wrap(null)
-  .usage(
-    pkg.name + ' v' + pkg.version + ' - Development server.\n' + 
-  '\nUsage:\n  ' + pkg.name + ' dev [options] [path]'
-  )
+  .option('V', {
+      alias: 'verbose',
+      description: 'Enables verbose output.'
+    })
+
+  .help('h', 'Show help.').alias('h', 'help')
+  .epilogue('  path           Path to a staticbuild.json file or directory to find one.\n' +
+  '                 If no path is supplied, the current directory is used.')
+  .argv;
+
+  console.dir(argv);
+}
+
+function runDevServer(yargs) {
+  argv = configureYargs(yargs)
+  .usage(commandUsage('dev', '[options] [path]', 'Development server.'))
   .option('n', {
     alias: 'norestart',
     description: 'Disables the built-in nodemon server restart.',
@@ -38,21 +47,41 @@ function (yargs) {
   .epilogue('  path           Path to a staticbuild.json file or directory to find one.\n' +
 '                 If no path is supplied, the current directory is used.')
   .argv;
-
+  
   console.log('running dev server...');
 
-})
+}
 
-.command('setup', 'Setup a new project.')
+function runSetup(yargs) {
+  argv = configureYargs(yargs)
+  .usage(commandUsage('setup', '[options] [path]', 'Setup.'))
+  .help('h', 'Show help.').alias('h', 'help')
+  .epilogue('  path           Path for a new staticbuild.json file or project directory.')
+  .argv;
+  
+  console.log('running setup...');
+}
 
-.option('V', {
-  alias: 'verbose',
-  description: 'Enables verbose output.'
-})
+// #region Helpers
 
-.help('h', 'Show help.').alias('h', 'help')
-.epilogue('  path           Path to a staticbuild.json file or directory to find one.\n' +
-'                 If no path is supplied, the current directory is used.')
-.argv;
+function commandUsage(command, options, description) {
+  return pkg.name + ' v' + pkg.version + ' - ' + description + 
+  '\n\nUsage:\n  ' + pkg.name + ' ' + command + ' ' + options;
+}
 
-console.dir(argv);
+function configureYargs(yargs) {
+  yargs = yargs || Yargs;
+  return yargs.wrap(null);
+}
+
+function mainUsage() {
+  return commandUsage('<command>', '[options] [path]', pkg.description);
+}
+
+function titleVersion() {
+  return pkg.name + ' v' + pkg.version;
+}
+
+// #endregion
+
+run();
